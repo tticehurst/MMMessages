44f5a5515f1d8594312ce6a196235ab18580482c
tticehurst <shnopy319@gmail.com>
2022-11-16 16:26:44 +0000

    fixed host issue

--- node_helper.js
+++ node_helper.js
@@ -6,41 +6,41 @@ const logger = require("logger");
 let oldMessage = undefined;
 
 module.exports = NodeHelper.create({
-	async socketNotificationReceived(id) {
-		if (id === "GetLastMessage") {
-			const lastMessage = (await axios.get("http://localhost:8081/lastMessage")).data;
-			this.sendSocketNotification("SetMessage", lastMessage || "");
-		}
-	},
+  async socketNotificationReceived(id) {
+    if (id === "GetLastMessage") {
+      const lastMessage = (await axios.get("http://localhost:8081/lastMessage")).data;
+      this.sendSocketNotification("SetMessage", lastMessage || "");
+    }
+  },
 
-	init() {
-		const fastify = require("fastify")({
-			logger: false,
-		});
+  init() {
+    const fastify = require("fastify")({
+      logger: false,
+    });
 
-		fastify.get("/setMessage", (req, reply) => {
-			const lowerCaseKeys = Object.keys(req.query).map((v) => v.toLowerCase());
-			const messageValue = req.query[lowerCaseKeys.find((v) => v === "message")] || undefined;
+    fastify.get("/setMessage", (req, reply) => {
+      const lowerCaseKeys = Object.keys(req.query).map((v) => v.toLowerCase());
+      const messageValue = req.query[lowerCaseKeys.find((v) => v === "message")] || undefined;
 
-			if (messageValue && messageValue.trim().length >= 1) {
-				this.sendSocketNotification("SetMessage", messageValue || "");
-				oldMessage = messageValue;
-				reply.send("Sent");
-			} else {
-				reply.send("Incorrect format");
-			}
-		});
+      if (messageValue && messageValue.trim().length >= 1) {
+        this.sendSocketNotification("SetMessage", messageValue || "");
+        oldMessage = messageValue;
+        reply.send("Sent");
+      } else {
+        reply.send("Incorrect format");
+      }
+    });
 
-		fastify.get("/lastMessage", (_req, reply) => {
-			reply.send(oldMessage);
-		});
+    fastify.get("/lastMessage", (_req, reply) => {
+      reply.send(oldMessage);
+    });
 
-		fastify.post("/cancelMessage", (_req, reply) => {
-			this.sendSocketNotification("SetMessage", "");
-			oldMessage = "";
-			reply.send("Message cancelled");
-		});
-		logger.log(global.config.port);
-		fastify.listen({ port: global.config.port + 1 });
-	},
+    fastify.post("/cancelMessage", (_req, reply) => {
+      this.sendSocketNotification("SetMessage", "");
+      oldMessage = "";
+      reply.send("Message cancelled");
+    });
+
+    fastify.listen({ port: global.config.port + 1, host: "0.0.0.0" });
+  },
 });
